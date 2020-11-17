9f5ef1aad898eb11c1c8b45308019b949f7be3cd
Ramon <ramonomello@gmail.com>
2020-11-16 21:22:23 -0300

    fix animation

--- styles/global.js
+++ styles/global.js
@@ -1,11 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  @keyframes fadein {
-    from { opacity: 0; }
-    to   { opacity: 1; }
-  }
-
   * {
     margin: 0;
     padding: 0;
@@ -19,8 +14,6 @@ const GlobalStyle = createGlobalStyle`
     font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     color: #282C34;
-    background: linear-gradient(270deg, #f1f1f1 0%, #f9f9f9 100%);
-    animation: fadein 2s;
   }
 
  a {
