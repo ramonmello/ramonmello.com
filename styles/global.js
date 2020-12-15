81bad03e0afd8f1e5fcb61eb031584b2b45c7322
Ramon <ramonomello@gmail.com>
2020-12-14 23:32:28 -0300

    Fix global font

--- styles/global.js
+++ styles/global.js
@@ -41,8 +41,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'Ubuntu';
     color: #282C34;
   }
 
