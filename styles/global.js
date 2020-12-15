f3648fad5402d6fb17c3995ce8cc5d39b23c6bd6
Ramon <ramonomello@gmail.com>
2020-12-15 00:33:26 -0300

    fix

--- styles/global.js
+++ styles/global.js
@@ -44,7 +44,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: "Ubuntu";
+    font-family: 'Ubuntu', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     color: #282C34;
   }
 
