22149041996e627cc3cad9b1ec01de002c2ac7e4
Ramon Mello <ramonomello@gmail.com>
2024-01-09 06:08:46 -0300

    feat: config theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		colors: {
+			black: '#000000',
+			gray: '#999999',
+			yellow: '#F2B32D',
+			orange: '#F8461D',
+			white: '#FFFFFF'
+		}
 	},
 	plugins: []
 };
