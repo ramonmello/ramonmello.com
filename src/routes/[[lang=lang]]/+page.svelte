06b2873be318a145076cea455fdfd0721f0eb805
Ramon Mello <ramonomello@gmail.com>
2024-01-12 08:51:16 -0300

    feat: add name component

--- src/routes/[[lang=lang]]/+page.svelte
+++ src/routes/[[lang=lang]]/+page.svelte
@@ -1,7 +1,13 @@
-<script lang="ts">
-	import Switch from '$lib/components/SwitchLang.svelte';
-	import * as m from '$paraglide/messages';
-</script>
-
-<Switch />
-<h1>{m.hello_world()}</h1>
+<main class="flex h-screen w-screen flex-col font-sans">
+	<section class="flex flex-1 flex-col items-center justify-center">
+		<div>
+			<h1 class="text-9xl font-bold">Ramon</h1>
+			<div class="flex items-center justify-between">
+				<svg width="85" height="85" viewBox="0 0 85 85" fill="none" class="mb-6 mr-5">
+					<circle cx="42.5" cy="42.5" r="42.5" fill="yellow" />
+				</svg>
+				<h1 class="text-9xl font-bold">Mello</h1>
+			</div>
+		</div>
+	</section>
+</main>
