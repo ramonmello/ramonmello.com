d04404be64e3f773a36f7ea6d80de3f6056bd504
Ramon Mello <ramonomello@gmail.com>
2024-01-13 07:43:59 -0300

    feat: hero component responsive

--- src/routes/[[lang=lang]]/+page.svelte
+++ src/routes/[[lang=lang]]/+page.svelte
@@ -5,13 +5,17 @@
 <main class="flex h-screen w-screen flex-col font-sans">
 	<section class="flex flex-1 flex-col items-center justify-center">
 		<div class="flex flex-col">
-			<p class="mb-8 ml-2 text-lg uppercase text-gray">{m.i_am()}</p>
-			<h1 class="text-9xl font-bold">Ramon</h1>
+			<p class="mb-4 ml-2 text-sm uppercase text-gray md:mb-8 md:text-lg">{m.i_am()}</p>
+			<h1 class="text-8xl font-bold md:text-9xl">Ramon</h1>
 			<div class="-mt-4 inline-flex w-full justify-between">
-				<img src="./images/avatar.jpeg" alt="" class="h-24 w-24 rounded-full" />
-				<h1 class="text-9xl font-bold">Mello</h1>
+				<img
+					src="./images/avatar.jpeg"
+					alt=""
+					class="mt-2 h-16 w-16 rounded-full md:mt-0 md:h-24 md:w-24"
+				/>
+				<h1 class="text-8xl font-bold md:text-9xl">Mello</h1>
 			</div>
-			<p class="mr-2 mt-3 self-end text-right text-lg uppercase text-gray">
+			<p class="mr-2 mt-1 self-end text-right text-sm uppercase text-gray md:mt-3 md:text-lg">
 				{@html m.a_software_developer()}
 			</p>
 		</div>
